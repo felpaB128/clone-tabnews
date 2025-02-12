9e073ca79dd46a9161a991151167be1d6adda515
Felipe <79150290+felpaB128@users.noreply.github.com>
2025-02-12 15:10:42 +0000

    make database.js client.connect() more roubust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,15 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credencias do Postgres");
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch {
     error;
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
